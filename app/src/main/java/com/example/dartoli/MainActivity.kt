f5c5ed768c7dbb8e750164b025fbd8c98b803e16
Valentin Marotta <ufpkv@student.kit.edu>
2023-10-05 09:39:11 +0200

    Add routing to empty multiplayer activity with view binding

--- app/src/main/java/com/example/dartoli/MainActivity.kt
+++ app/src/main/java/com/example/dartoli/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.dartoli
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dartoli.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnTraining.setOnClickListener {
+            startActivity(Intent(this@MainActivity, MultiplayerActivity::class.java))
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/dartoli/MultiplayerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dartoli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MultiplayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multiplayer)
+    }
+}
