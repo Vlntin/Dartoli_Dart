8e95bfaf3a857dd191424ffe1cb14761d084e420
Valentin Marotta <ufpkv@student.kit.edu>
2023-10-23 12:46:07 +0200

    create new branch

--- app/src/main/java/com/example/dartoli/activities/CountingGameActivity.kt
+++ app/src/main/java/com/example/dartoli/activities/CountingGameActivity.kt
@@ -3,10 +3,16 @@ package com.example.dartoli.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dartoli.R
+import com.example.dartoli.databinding.ActivityCountingGameBinding
+import com.example.dartoli.databinding.ActivityCricketBinding
 
 class CountingGameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCountingGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_counting_game)
+        binding = ActivityCountingGameBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
